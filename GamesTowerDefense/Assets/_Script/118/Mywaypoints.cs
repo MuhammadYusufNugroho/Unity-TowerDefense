3e6d73af2aba5f02b8d97c19b2a20ab98bfa8995
`Yusuf <muhammadyusufn1507@gmail.com>
2022-10-29 11:28:46 +0700

    Update Waypoint script and creating TargetLocator Script

---
+++ GamesTowerDefense/Assets/_Script/118/Mywaypoints.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mywaypoints : MonoBehaviour
+{
+    // Variable for tower prefab and logic for placeable
+    [SerializeField] GameObject _towerPrefab;
+    [SerializeField] bool _isPlaceable;
+
+    private void OnMouseDown()
+    {
+        if (_isPlaceable)
+        {
+            //Debug.Log(transform.name);
+            Instantiate(_towerPrefab, transform.position, Quaternion.identity);
+            // Logic for only place one tower each location
+            _isPlaceable = false;
+        }
+    }
+}
