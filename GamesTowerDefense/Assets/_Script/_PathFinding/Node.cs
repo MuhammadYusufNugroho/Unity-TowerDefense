1a5aaaabf9981bfa52fdbf2581bb079dce8b0970
`Yusuf <muhammadyusufn1507@gmail.com>
2022-11-02 19:00:02 +0700

    Checking bugs

--- GamesTowerDefense/Assets/_Script/_PathFinding/Node.cs
+++ GamesTowerDefense/Assets/_Script/_PathFinding/Node.cs
@@ -1,18 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
 public class Node
 {
-    // Creating pure variable for access in another class
     public Vector2Int coordinates;
     public bool isWalkable;
     public bool isExplored;
     public bool isPath;
     public Node connectedTo;
-    // Constructor for Variable Vector2Int and bool isWalkable
-    public Node(Vector2Int _coordinates, bool _isWalkable)
+
+    public Node(Vector2Int coordinates, bool isWalkable)
     {
-        this.coordinates = _coordinates;
-        this.isWalkable = _isWalkable;
+        this.coordinates = coordinates;
+        this.isWalkable = isWalkable;
     }
 }
--- GamesTowerDefense/Assets/_Script/_TowerSystem/Tower.cs
+++ GamesTowerDefense/Assets/_Script/_TowerSystem/Tower.cs
@@ -1,27 +1,55 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    // Variables for cost
-    [SerializeField] int _cost = 75;
+    [SerializeField] int cost = 75;
+    [SerializeField] float buildDelay = 1f;
 
-    // Creating method CreateTower to Waypoints Script, we are using bool cuz later on we're gonna set this to false
+    void Start()
+    {
+        StartCoroutine(Build());
+    }
+
     public bool CreateTower(Tower tower, Vector3 position)
     {
-        // Reference to Banking Class
-        Banking m_Banking = FindObjectOfType<Banking>();
-
-        if (m_Banking == null) { return false; }
-
-        if (m_Banking.CurrentBalance >= _cost)
+        Bank bank = FindObjectOfType<Bank>();
+        
+        if(bank == null)
         {
-            Instantiate(tower.gameObject, position, Quaternion.identity);
-            m_Banking.Withdraw(_cost);
-            return true;
+            return false;
+        }
+
+        if(bank.CurrentBalance >= cost)
+        {
+            Instantiate(tower, position, Quaternion.identity);
+            bank.Withdraw(cost);
+            return true; 
         }
 
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildDelay);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
     }
 }
