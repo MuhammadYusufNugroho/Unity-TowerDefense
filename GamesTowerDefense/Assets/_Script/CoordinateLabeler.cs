8573af1746ae942333a05cb2eaf15bf35778ecc9
`Yusuf <muhammadyusufn1507@gmail.com>
2022-11-18 19:41:04 +0700

    Recreating CoordinateLabel

--- GamesTowerDefense/Assets/_Script/CoordinateLabeler.cs
+++ GamesTowerDefense/Assets/_Script/CoordinateLabeler.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
+[ExecuteAlways]
 public class CoordinateLabeler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Variable for Textmeshpro
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+    private void Awake()
     {
-        
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinate();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinate();
+            UpdateObjectName();
+        }
+    }
+
+    private void DisplayCoordinate()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+
+    private void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
     }
 }
