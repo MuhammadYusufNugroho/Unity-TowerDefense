d9de6caab378143f7d414e0710ac91392af91bc0
`Yusuf <muhammadyusufn1507@gmail.com>
2022-11-19 11:00:10 +0700

    Creating difficulties when enemy dies

--- GamesTowerDefense/Assets/_Script/_BankingSystem/Bank.cs
+++ GamesTowerDefense/Assets/_Script/_BankingSystem/Bank.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -11,23 +12,34 @@ public class Bank : MonoBehaviour
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
 
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     private void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
+
         if (currentBalance < 0)
         {
             ReloadScene();
         }
+    }
+
+    private void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     private void ReloadScene()
